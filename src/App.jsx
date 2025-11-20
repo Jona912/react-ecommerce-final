ba4fc7407caa2ec80dd53853d32a29aa220e97c4
Bruno Harari <jonathansgonzalez22@gmail.com>
2025-11-19 21:51:45 -0300

    package homepage cambiado

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ export default function App() {
   return (
     <ProductProvider>
     <CartProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/react-ecommerce-final">
         <NavBar />
         
         <Routes>
